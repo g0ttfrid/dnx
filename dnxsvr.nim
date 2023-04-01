5345a613b86d06fdb97b394ae18e7db46b311189
g0ttfrid <yurimaia@protonmail.ch>
2023-04-01 18:38:15 -0300

    Update dnxsvr.nim

--- dnxsvr.nim
+++ dnxsvr.nim
@@ -1,4 +1,5 @@
-import net, strutils, asyncnet, asyncdispatch
+import net, strutils, asyncnet, asyncdispatch, random
+randomize()
 
 let withSize = 256
 var socket = newAsyncSocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
@@ -6,22 +7,29 @@ socket.bindAddr(port = Port 53)
 
 echo "[+] Listening..."
 
-var query: string
+var
+    query: string
+    name: string
 while true:
     let 
         req = waitfor socket.recvFrom(withSize)
         data = $req.data
         x = data.toHex[26 .. ^1]
-    
+    #echo "[DEBUG] ", x
     if x.contains("71756974"):
         break
+    elif x.contains("02626211"):
+        #echo "[DEBUG] ", parseHexStr(x[0 .. ^61])
+        name.add(parseHexStr(x[0 .. ^61]))
     elif x.contains("016103"):
+        #echo "[DEBUG] ", parseHexStr(x[0 .. ^61])
         query.add(parseHexStr(x[0 .. ^61]))
-        
+       
 try:
-    let file = parseHexStr(query)
-    writeFile("new.file", $file)
-    echo "[+] Received: new.file (Sorry, rename it!)"
-    #echo file
+    let 
+        content = parseHexStr(query)
+        filename = $rand(10000) & parseHexStr(name)
+    writeFile(filename, $content)
+    echo "[+] Received: ", filename, " [lengh: ", content.len, "]"
 except CatchableError as e:
     echo "[!] Error: ", e.msg
